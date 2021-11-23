65180e0ccf32d87c8e5d317ab0f3d974c6a4c527
Andrey Larikov <larikov174@gmail.com>
2021-11-23 15:59:51 +0500

    feat: add scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Header } from "./components/header/Header";
-import { FeedbackBanner } from "./components/feedbackBanner/FeedbackBanner";
-import { TagArea } from "./components/tagArea/TagArea";
-import { Roadmap } from "./components/roadmap/Roadmap";
-import { Hamburger } from "./components/hamburger/Hamburger";
-import { EmptyFeed } from "./components/emptyFeed/EmptyFeed";
+// import { FeedbackBanner } from "./components/feedbackBanner/FeedbackBanner";
+// import { TagArea } from "./components/tagArea/TagArea";
+// import { Roadmap } from "./components/roadmap/Roadmap";
+// import { Hamburger } from "./components/hamburger/Hamburger";
+// import { EmptyFeed } from "./components/emptyFeed/EmptyFeed";
 
 function App() {
   return (
@@ -13,20 +13,31 @@ function App() {
         <section className="menu">
           {/* первый квадрат и заголовок */}
           <Header />
-          {/* навигация по тегам */}
-          <TagArea />
-          {/* роадмэп данные */}
-          <Roadmap />
+
         </section>
-        {/* гамбургер */}
-        <Hamburger />
-        {/* синяя шапка с сротировкой фидов */}
-        <FeedbackBanner />
-        {/* пустая страцина без фидбэков */}
-        <EmptyFeed />
+
       </article>
     </div>
   );
 }
 
 export default App;
+
+
+// <article className="container-main">
+// <section className="menu">
+//   {/* первый квадрат и заголовок */}
+//   <Header />
+//   {/* навигация по тегам */}
+//   <TagArea />
+//   {/* роадмэп данные */}
+//   <Roadmap />
+// </section>
+// {/* гамбургер */}
+// <Hamburger />
+// {/* синяя шапка с сротировкой фидов */}
+// <FeedbackBanner />
+// {/* пустая страцина без фидбэков */}
+// <EmptyFeed />
+// </article>
+
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header class="header-menu">
-      <h1 class="header-menu__title">Frontend Mentor</h1>
-      <h2 class="header-menu__subtitle">Feedback Board</h2>
-      <button class="header-menu__hamburger"></button>
+    <header class="header">
+      <h1 class="header__title">Frontend Mentor</h1>
+      <h2 class="header__subtitle">Feedback Board</h2>
+      <button class="header__hamburger-button"></button>
     </header>
   );
 };
